c45555d0223c3a611ad3e560d05fb69df7b06884
Chaos-And-Art <thechaosandart@gmail.com>
2022-01-12 12:19:14 -0700

    Update 0.1.4

--- src/app/components/custom-image/custom-image.component.ts
+++ src/app/components/custom-image/custom-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CustomImage } from 'src/app/models/customImage';
 
